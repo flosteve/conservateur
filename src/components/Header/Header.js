4823b68cb53fb73903c3460bedff7e5de560de52
flostevebron <eravillesteve@gmail.com>
2019-11-27 17:10:25 +0100

    Main Menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,24 +1,55 @@
 import React from "react";
-import { AppBar, Toolbar, Grid, IconButton, Link } from "@material-ui/core";
+import {
+  AppBar,
+  Toolbar,
+  Grid,
+  IconButton,
+  Menu,
+  MenuItem
+} from "@material-ui/core";
 import logo from "../../assets/icons/logo-conservateur.png";
 import connexion from "../../assets/icons/connexion.png";
 import SearchBar from "../SearchBar/SearchBar";
 import HeaderTabs from "../HeaderTabs/HeaderTabs";
 
 const Header = () => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <AppBar position="static" className="header-top">
       <Toolbar className="header-container">
         <Grid container className="header-main-grid">
           <Grid item xs={6} md={10}>
-            <Link href="/">
+            <IconButton href="/">
               <img src={logo} alt="logo-conservateur" />
-            </Link>
+            </IconButton>
           </Grid>
           <Grid item xs={6} md={2} className="header-connexion">
-            <IconButton>
+            <IconButton
+              onClick={handleClick}
+              aria-controls="main-menu"
+              aria-haspopup="true"
+            >
               <img src={connexion} alt="logo-connexion" />
             </IconButton>
+            <Menu
+              id="main-menu"
+              anchorEl={anchorEl}
+              keepMounted
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+              anchorPosition={{ top: 20 }}
+            >
+              <MenuItem onClick={handleClose}>Paramètres</MenuItem>
+              <MenuItem onClick={handleClose}>se déconnecter</MenuItem>
+            </Menu>
           </Grid>
         </Grid>
       </Toolbar>
